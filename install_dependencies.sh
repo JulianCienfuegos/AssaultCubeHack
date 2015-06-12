e2ba4ca3349a2e802b78e973f3dd1b6ebfc95b5b
JulianCienfuegos <melvyniandrag@gmail.com>
2015-06-11 23:31:48 -0400

    added clang and build-essential to dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,3 @@
-sudo apt-get install libvorbis-dev libopenal-dev libsdl-image1.2-dev libcurl4-openssl-dev libogg-dev libsdl1.2-dev libsdl1.2debian
+sudo apt-get install clang build-essential libvorbis-dev libopenal-dev libsdl-image1.2-dev libcurl4-openssl-dev libogg-dev libsdl1.2-dev libsdl1.2debian
 
 
